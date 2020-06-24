e84305832c622d9782b0c2ee7351b342fc3e0614
Martin Mraz <martin.mraz@alot-solutions.com>
2020-06-24 15:57:53 +0200

    Novy clanok v News: ALOT Solutions announces new appointment Head of Legal and Compliance.

---
+++ news-alot-solutions-announces-new-appointment-head-of-legal-and-compliance.php
@@ -0,0 +1,44 @@
+<?php 
+    include 'config.php';
+
+    $title = "ALOT Solutions announces new appointment Head of Legal and Compliance";
+    $meta_desc = "ALOT Solutions has today announced the appointment of Abigail Wahnon as Head of Legal and Compliance.";
+    $meta_keywords = "News, article, update, blog, ALOT";
+    $og_image = "";
+    $og_url = $url."/article".$ext;
+
+    include 'header.php';
+    include 'menu.php';
+?>
+
+<section class="section articles">
+    <div class="articles__header articles__header--overlap">
+        <div class="container">
+            <h1 class="heading-1">ALOT Solutions announces new appointment Head of Legal and Compliance</h1>
+        </div>
+    </div>
+    <div class="articles__content">
+        <div class="container">
+            <div class="articles__hero" style="background-image: url(i/article-hero-4.jpg);"></div>
+            <article class="articles__text">
+                <p class="date">24<sup>th</sup> of June, 2020</p>
+                <p>ALOT Solutions has today announced the appointment of Abigail Wahnon as Head of Legal and Compliance. This appointment will support the business as it continues to expand in the B2B and B2G market providing prize cover, technology and content to a wide range of customers.</p>
+                <p>Abigail brings with her over 10 years of corporate legal experience, having worked in Senior Management, Legal and Regulatory positions in Gibraltar, Spain and the UK. She will be responsible for legal, company secretarial and compliance functions at ALOT Solutions and will help to support and contribute to the continued growth of the business.</p>
+                <p>Abigail Wahnon commented: “I’m really pleased to be joining the ALOT Solutions team and can’t wait to meet everyone and get stuck in. Having been based in Gibraltar, I’ve been able to watch the company develop - I look forward to helping deliver the strategy and seeing the business grow.”</p>
+                <p>CEO Michael Carruthers commented: “Earlier this year ALOT Solutions launched to offer our prize cover, technology and content to a diverse range of operators. Having someone of Abigail’s calibre join our team will help us to grow quickly and safely as we power innovation in the lottery, gaming and prize promotion sectors to benefit players, operators and their good causes.”</p>
+                <p>Please note ALOT Solutions will be attending these industry events. Meetings with our representatives can be arranged by contacting
+                    <a href="mailto:info@alot-solutions.com">info@alot-solutions.com</a>:</p>
+                <ul>
+                    <li>10<sup>th</sup> to 11<sup>th</sup> November: B2B Marketing Expo, Excel London UK</li>
+                    <li>17<sup>th</sup> to 19<sup>th</sup> November: Sigma, Malta</li>
+                    <li>2<sup>nd</sup> to 4<sup>th</sup> February 2021: ICE, Excel London UK</li>
+                </ul>
+            </article>
+            <div class="articles__button-holder">
+                <a href="news<?=$ext?>" class="button button--blue">Back to news</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'footer.php'; ?>
