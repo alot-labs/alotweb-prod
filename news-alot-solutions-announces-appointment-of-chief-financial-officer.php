a20f315f12d2b29f409c5124885a1b91cac01d85
Martin Mraz <martin.mraz@alot-solutions.com>
2020-08-11 14:24:38 +0200

    new article CFO v2

---
+++ news-alot-solutions-announces-appointment-of-chief-financial-officer.php
@@ -0,0 +1,37 @@
+<?php 
+    include 'config.php';
+
+    $title = "ALOT Solutions announces appointment of Chief Financial Officer";
+    $meta_desc = "ALOT Solutions today announces the appointment of Mr. Geaspar Byrne as Chief Financial Officer of the Company, effective 1st August, 2020. Geaspar has been a senior consultant to ALOT Solutions and its predecessor, Lottoland Solutions, since 2017.";
+    $meta_keywords = "News, article, update, blog, ALOT";
+    $og_image = "";
+    $og_url = $url."/article".$ext;
+
+    include 'header.php';
+    include 'menu.php';
+?>
+
+<section class="section articles">
+    <div class="articles__header articles__header--overlap">
+        <div class="container">
+            <h1 class="heading-1">ALOT Solutions announces appointment of Chief Financial Officer</h1>
+        </div>
+    </div>
+    <div class="articles__content">
+        <div class="container">
+            <div class="articles__hero" style="background-image: url(i/article-hero-6.jpg);"></div>
+            <article class="articles__text">
+                <p class="date">11<sup>th</sup> of August, 2020</p>
+                <p>ALOT Solutions today announces the appointment of Mr. Geaspar Byrne as Chief Financial Officer of the Company, effective 1<sup>st</sup> August, 2020.</p>
+                <p>Geaspar has been a senior consultant to ALOT Solutions and its predecessor, Lottoland Solutions, since 2017. He has more than 21 years’ experience of corporate finance, accounting, internal control, corporate governance, risk, strategy, business development and growth management across international markets. Prior to 2017, he held senior positions with leading global institutions including Alibaba Group (AGTech Holdings) and Deutsche Bank. Geaspar is a Chartered Financial Analyst, holds a degree in Financial & Business Economics and is a member of the Financial Times Non-Executive Directors' Club.</p>
+                <p>Commenting on the appointment, Michael Carruthers, CEO of ALOT Solutions said: “I am delighted to welcome Geaspar to the role of Chief Financial Officer. Geaspar will play a leading role in shaping and delivering our strategy and will ensure that our finance function continues to develop in a way that supports our ambitious growth plans. At a time when our development is accelerating, Geaspar brings an ideal combination of in-depth understanding of the business as well as significant corporate finance experience.”</p>
+                <p>Geaspar Byrne said: “I am very excited to be expanding my role with ALOT at this important time in the Company’s development. I look forward to working closely with Mike and the rest of the executive team to facilitate the successful delivery of our strategy.”</p>
+            </article>
+            <div class="articles__button-holder">
+                <a href="news<?=$ext?>" class="button button--blue">Back to news</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'footer.php'; ?>
